7fad32c828966fac838a7c48863a58ed9c18a792
puran05 <77312684+puran05@users.noreply.github.com>
2024-04-21 12:55:05 -0500

    done

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello from Home</h2>
+      <h3>I am inside the home. COzy</h3>
+    </div>
+  );
+}
